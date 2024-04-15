bcc97b82b45350fd95ff9a937ad04f6ea8ef3819
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2024-04-15 22:44:37 +0200

    Add methods

--- 05-ArrayMethod/05-ArrayMethod.js
+++ 05-ArrayMethod/05-ArrayMethod.js
@@ -1,7 +1,7 @@
 //--------------------------------------------------------------
 //                            .map()
 //             Loops and returns an array[] (check line26)
-//             Syntax : arrray.map(variableName => action)
+//             Syntax : arrray.map(elementName => action)
 //--------------------------------------------------------------
 
 // // the whole function 
@@ -39,9 +39,75 @@ console.log(doubleMap([35,36,37,38])); // [ 70, 72, 74, 76 ]
 
 //--------------------------------------------------------------
 //                        .filter()
-//           Loops and returns an array[] (check line26)
-//           Syntax : arrray.map(variableName => action)
+//           Loops and returns an array[] with matching conditions (Line 59)
+//           Syntax : arrray.filter(variableName => action/condition)
 //--------------------------------------------------------------
+
+const filter =(numbers, graterThan) => {
+    let result = [];
+    for (const number of numbers){ // better to set number as "const"
+        if (number > graterThan){ 
+            result.push(number); 
+        }
+    }
+    return result;
+}
+
+const nums = [3,5,18,19,21,89,50,4,58,20]; 
+console.log(filter(nums, 10)); //[ 89, 50, 58, 20 ]
+
+let resultFilter1 = nums.filter(num => num > 10); 
+console.log(resultFilter1); // [ 89, 50, 58, 20 ]
+
+let resultFilter2 = nums.filter(num => num > 10 && num < 20); 
+console.log(resultFilter2);
+
+// if (userIsAuthenticated || userIsPayingMmber) {
+//     show youtube videos
+// } else{
+//    
+// }
+
+const actors = [
+    {name : "Vanessa", netWorth: 200000000000000},
+    {name : "Johnny", netWorth : 1000},
+    {name : "Leonardo", netWorth : 100000000000}
+]
+
+let result3= actors.filter(actor => actor.netWorth > 100000)
+console.log(result3); 
+// { name: 'Vanessa', netWorth: 200000000000000 },
+// { name: 'Leonardo', netWorth: 100000000000 }
+
+//--------------------------------------------------------------
+//                        .join(' deliminator ')
+//       put into a string seperated by the deliminator 
+//        
+//--------------------------------------------------------------
+
+let names = result3.map(actor => actor.name).join(', '); 
+console.log(names); 
+// Vanessa, Leonardo
+
+
+//--------------------------------------------------------------
+//                        .reduce()
+//        loops and gives you back the accumulator
+//     Higher over function: take a function ad an argument
+//      Syntax = array.reduce((ele1,ele2) => action )
+//--------------------------------------------------------------
+// 3.27.29
+
+const nums1 = [1,2,3]; 
+const result4 = nums1.reduce(function(prev,curr) {
+    return prev + curr;
+})
+
+const result5 = nums1.reduce((prev, curr) => prev + curr) // implicit return
+
+console.log(result4);
+console.log(result5);
+
 
 
 
