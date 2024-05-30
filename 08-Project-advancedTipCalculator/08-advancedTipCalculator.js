eb605c8795e0b0a2a42d72fa688717628c0ff268
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2024-05-30 23:00:53 +0200

    Add Global access to input + calculateBill Function

--- 08-Project-advancedTipCalculator/08-advancedTipCalculator.js
+++ 08-Project-advancedTipCalculator/08-advancedTipCalculator.js
@@ -19,31 +19,36 @@ These are all the DIV ID's you're gonna need access to 👇
 
 // Get global access to all inputs / divs here (you'll need them later 😘)
 // bill input, tip input, number of people div, and per person total div
-
-
+const billTotalInputDiv = document.getElementsById("billTotalInput");
+const tipInputDiv = document.getElementById("tipInput");
+const numberOfPeopleDiv = document.getElementById("numberOfPeople");
+const perPersonTotalDiv = document.getElementById("perPersonTotal");
 
 // Get number of people from number of people div
+numPeople = Number(numberOfPeopleDiv.innerText);  
+
 
 
 // ** Calculate the total bill per person **
 const calculateBill = () => {
     // get bill from user input & convert it into a number
-    
+    totalBill = Number(billTotalInputDiv.innerText);
+
   
     // get the tip from user & convert it into a percentage (divide by 100)
-    
+    tipPercentage = Number(tipInputDiv.innerText) / 100; 
   
     // get the total tip amount
-    
+    totalTip = totalBill * tipPercentage; 
   
     // calculate the total (tip amount + bill)
-    
+    totalAmount = totalBill + totalTip; 
   
     // calculate the per person total (total divided by number of people)
-  
+    totalPerPerson = totalAmount / numPeople;
   
     // update the perPersonTotal on DOM & show it to user
-  
+    perPersonTotalDiv.innerText = totalPerPerson; 
   }
   
   // ** Splits the bill between more people **
