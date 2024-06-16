9abfa9e6df9f08767e57c84fe74c423ffb291d02
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2024-06-16 23:21:22 +0200

    Define and add token to Fetch Request

--- todo-list-with-backend/script.js
+++ todo-list-with-backend/script.js
@@ -1,10 +1,28 @@
 // {JSON} Placeholder API : https://jsonplaceholder.typicode.com/
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+// Define and add token to Fetch Request 
+// token : 09W8XKZHKdOfEbC9d3FLqYT2pwnm4S0Xoe6kW4me
+
+// headers: { ... }: This specifies the HTTP headers to be included with the request. 
+//                   Headers provide additional information about the request, such as 
+//                   the type of content being sent or the type of response expected.
+
+// 'Authorization': Bearer ${apiToken}`:
+//       `Authorization` header is commonly used to send credentials to authenticate a user or a service.
+//       `Bearer` is a type of token commonly used for authentication.
+
+const apiUrl = "https://jsonplaceholder.typicode.com/todos";
+const myToken= "09W8XKZHKdOfEbC9d3FLqYT2pwnm4S0Xoe6kW4me "; 
+
+fetch(apiUrl,{
+    headers : {
+        "authorization" : `Bearer ${myToken}`
+    } 
+})
     .then(response => response.json())
     .then(json => {
         console.log("RunJson");
-        console.log(json.message);
+        console.log(json)
     })
 
 // Add Task c
